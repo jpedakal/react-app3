dee27943aececa7c170afca64afb70176c1ab002
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-12 18:46:05 +0530

    update

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Axios from 'axios';
+
+const url= ""
 
 class Artist extends Component {
+  constructor(){
+      super()
 
+      this.state={
+          details: ""
+      }
+  }
     render() {
         return (
             <div>
                 <Header />
             </div>
         )
+    }
+
+    componentDidMount(){
+        
     }
 }
 
