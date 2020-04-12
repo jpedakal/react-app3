ac1bdc325e7a2987586ece77d26e36a9ecd417e5
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-12 11:44:43 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,32 @@
 import React, { Component, Fragment } from 'react';
 import Banner from './Banner';
 
+const url = "http://localhost:5000/artist";
+
 class Home extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            artist: ""
+        }
+    }
     render() {
         return (
             <Fragment>
                 <Banner />
             </Fragment>
         )
+    }
+
+    componentDidMount() {
+        fetch(url, { method: "GET" })
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({
+                    artist: data
+                })
+            })
     }
 }
 
