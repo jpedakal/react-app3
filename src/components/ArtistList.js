566bf7305644c3602a53884ea789ea22b3c41d13
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-12 15:02:55 +0530

    Update ArtistList.js

---
+++ src/components/ArtistList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ArtistList = () => {
+    return (
+        <div className="artist_list">
+            <h4>List of Artists</h4>
+        </div>
+    )
+}
