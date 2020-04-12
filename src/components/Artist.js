283d3b707de2f7d896f0c790b0b6c49cbf5fefbc
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-12 18:30:22 +0530

    Update Artist.js

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-
+import Header from './Header';
 
 class Artist extends Component {
 
     render() {
         return (
             <div>
-
+                <Header />
             </div>
         )
     }
