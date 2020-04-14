08f2993c6ead17246ff810ee583f9d9e2acb8ed5
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-14 09:04:31 +0530

    Update formComponent.js

--- src/components/formComponent.js
+++ src/components/formComponent.js
@@ -16,14 +16,34 @@ class FormsComponent extends Component {
         return (
             <Fragment>
                 <Header />
-                <div className="panel panel-primary">
-                    <div className="panel-heading">
-                        Artist Forms
+                <div className="container">
+                    <div className="panel panel-primary">
+                        <div className="panel-heading">
+                            Artist Forms
                    </div>
-                    <div className="panel-body">
-                        <div className="form-group">
-                           <label>Name</label>
-                           <input type="text" className="form-group" value={this.state.name}></input>
+                        <div className="panel-body">
+                            <div className="form-group">
+                                <label>Name:</label>
+                                <input type="text" className="form-group" value={this.state.name} onChange={this.handleChangeName}></input>
+                            </div>
+                        </div>
+                        <div className="panel-body">
+                            <div className="form-group">
+                                <label>Cover:</label>
+                                <input type="text" className="form-group" value={this.state.cover} onChange={this.handleChangeCover}></input>
+                            </div>
+                        </div>
+                        <div className="panel-body">
+                            <div className="form-group">
+                                <label>Bio:</label>
+                                <input type="text" className="form-group" value={this.state.bio} onChange={this.handleChangeBio}></input>
+                            </div>
+                        </div>
+                        <div className="panel-body">
+                            <div className="form-group">
+                                <label>Genre:</label>
+                                <input type="text" className="form-group" value={this.state.genre} onChange={this.handleChangeGenre}></input>
+                            </div>
                         </div>
                     </div>
                 </div>
