7880e5a702c8cb74b58c0933777f25b960a7e912
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-14 08:53:18 +0530

    Update formComponent.js

--- src/components/formComponent.js
+++ src/components/formComponent.js
@@ -2,10 +2,26 @@ import React, { Component, Fragment } from 'react';
 import Header from './Header';
 
 class FormsComponent extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            id: "",
+            name: "",
+            cover: "",
+            bio: "",
+            genre: ""
+        }
+    }
     render() {
         return (
             <Fragment>
                 <Header />
+                <div className="panel panel-primary">
+                    <div className="panel-heading">
+                        Artist Forms
+                   </div>
+                </div>
             </Fragment>
         )
     }
