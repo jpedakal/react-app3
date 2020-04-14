ff24d8b94d6558153059ae0f7a03e53092676a67
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-14 08:30:21 +0530

    Create formComponent.js

---
+++ src/components/formComponent.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+
+class FormsComponent extends Component {
+    render() {
+        return (
+            <Fragment>
+                <div>
+
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+export default FormsComponent;
